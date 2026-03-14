9bebcf2da98c5d537ee6e61cc21782dee797688b
BhanuSri-19 <bhanusrik7@gmail.com>
2026-03-14 21:33:54 +0530

    Fix API URL for production

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+/*import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: "https://vehicle-service-backend-fmet.onrender.com"
 });
 
 // Login API
@@ -17,5 +17,22 @@ export const bookService = (data) =>
   API.post("/booking/add", data);
 
 // Get service history
+export const getServiceHistory = (customerId) =>
+  API.get(`/booking/history/${customerId}`);*/
+import axios from "axios";
+
+const API = axios.create({
+  baseURL:"https://vehicle-service-backend-fmet.onrender.com"
+});
+
+export const login = (data) =>
+  API.post("/api/login", data);
+
+export const register = (data) =>
+  API.post("/api/register", data);
+
+export const bookService = (data) =>
+  API.post("/booking/add", data);
+
 export const getServiceHistory = (customerId) =>
   API.get(`/booking/history/${customerId}`);
